a76a4827a9ce82cf808af2a3e8d2b0db2fd4fae0
xikyzz <harshasnani41@gmail.com>
2025-04-04 11:51:49 +0530

    Create valid_sudoku.cpp

---
+++ valid_sudoku.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValidSudoku(std::vector<std::vector<char>>& board) {
+        std::vector<std::unordered_set<char>> rows(9), cols(9), boxes(9);
+        
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num == '.') continue;
+                
+                int boxIndex = (i / 3) * 3 + (j / 3);
+                
+                if (rows[i].count(num) || cols[j].count(num) || boxes[boxIndex].count(num)) {
+                    return false;
+                }
+                
+                rows[i].insert(num);
+                cols[j].insert(num);
+                boxes[boxIndex].insert(num);
+            }
+        }
+        return true;
+    }
+};
