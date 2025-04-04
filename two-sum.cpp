0e4e8e6baaea0daaec2be1bc5f330f5274c8b5e8
xikyzz <harshasnani41@gmail.com>
2025-04-04 11:43:32 +0530

    Create two-sum.cpp

---
+++ two-sum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> numMap;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+        return {}; 
+    }
+};
+
+int main() {
+    Solution solution;
+    std::vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    std::vector<int> result = solution.twoSum(nums, target);
+    
+    std::cout << "Indices: [" << result[0] << ", " << result[1] << "]" << std::endl;
+    return 0;
+}
